023c5eccb43942068acf0f1af63c06d05d248274
R-8211 <coldplayavicii8211@gmail.com>
2026-05-10 18:28:40 +0900

    chore: 2026-05-10 18:28

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT ?? 3001
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: /^http:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 }))
